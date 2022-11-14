1e9f63273817bedaec43fecf311b5f53c168b81d
MagicalSheep <wen_cheng_ke@126.com>
2022-11-14 01:21:51 +0000

    improve error message

--- src/handlers.rs
+++ src/handlers.rs
@@ -7,13 +7,21 @@ use crate::{ADAPTER, MODEL};
 use casbin::{CoreApi, Enforcer};
 
 use jsonwebtoken::{Algorithm, DecodingKey, Validation};
-use log::debug;
+use log::{debug, error};
 use warp::hyper::StatusCode;
+use warp::reject::Reject;
 use warp::{reject, reply, Rejection, Reply};
 
 use crate::entity::CasdoorUser;
 use crate::response::{ActiveResponse, TokenResponse};
 use crate::{CLIENT, CONFIG};
+
+#[derive(Debug)]
+struct CustomRejection {
+    msg: String,
+}
+
+impl Reject for CustomRejection {}
 
 /// Parse jwt token to casdoor user entity.
 fn parse_jwt_token(token: &str) -> Result<CasdoorUser, Box<dyn std::error::Error>> {
@@ -29,17 +37,39 @@ fn parse_jwt_token(token: &str) -> Result<CasdoorUser, Box<dyn std::error::Error
 #[cfg(feature = "builtin-casbin")]
 async fn enforce(token: String, path: String, method: String) -> Result<bool, Rejection> {
     // User's organization and name should not be changed by updating profile.
-    // So that a valid access_token can always get the valid id (owner/name)
-    let user = parse_jwt_token(&token).map_err(|_| reject::reject())?;
+    // So that a valid access_token can always get the valid id (owner/name).
+    // Token should be valid when enforce permission control since authentication completed.
+    let user = parse_jwt_token(&token).map_err(|err| {
+        error!("{}", err);
+        reject::custom(CustomRejection {
+            msg: "Unexpected token when enforce permission control".to_string(),
+        })
+    })?;
     let sub = format!("{}/{}", user.owner, user.name);
 
-    let model = MODEL.get().ok_or(reject::reject())?;
-    let adapter = ADAPTER.get().ok_or(reject::reject())?;
-    let enforcer = Enforcer::new(model.clone(), adapter.clone()).await.unwrap();
+    let model = MODEL.get().ok_or(reject::custom(CustomRejection {
+        msg: "Get permission model from memory failed (None Model)".to_string(),
+    }))?;
+    let adapter = ADAPTER.get().ok_or(reject::custom(CustomRejection {
+        msg: "Get permission adapter from memory failed (None Adapter)".to_string(),
+    }))?;
+    let enforcer = Enforcer::new(model.clone(), adapter.clone())
+        .await
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection {
+                msg: "Build enforcer from permission model and adapter failed".to_string(),
+            })
+        })?;
 
     let res = enforcer
         .enforce((sub, path, method.to_lowercase()))
-        .map_err(|_| reject::reject())?;
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection {
+                msg: "Enforce permission controll failed".to_string(),
+            })
+        })?;
 
     Ok(res)
 }
@@ -62,13 +92,28 @@ async fn enforce(token: String, path: String, method: String) -> Result<bool, Re
         .header("Authorization", format!("Bearer {}", token))
         .send()
         .await
-        .map_err(|_| reject::reject())?;
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection {
+                msg: "Request for casdoor api \"/api/enforce\" failed".to_string()
+            })
+        })?;
     let res = resp
         .text()
         .await
         .map(|s| s.parse::<bool>())
-        .map_err(|_| reject::reject())?
-        .map_err(|_| reject::reject())?;
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection { 
+                msg: "Deserialize response from casdoor api \"/api/enforce\" to text failed".to_string() 
+            })
+        })?
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection { 
+                msg: "Parse text to bool type failed".to_string() 
+            })
+        })?;
 
     Ok(res)
 }
@@ -81,7 +126,12 @@ pub async fn handle_login(query: HashMap<String, String>) -> Result<impl Reply, 
         .post(format!("{}/api/login/oauth/access_token?grant_type=authorization_code&client_id={}&client_secret={}&code={}", CONFIG.endpoint, CONFIG.client_id, CONFIG.client_secret, code))
         .send()
         .await
-        .map_err(|_| reject::reject())?
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection { 
+                msg: "Request for casdoor api \"/api/login/oauth/access_token\" failed".to_string()
+            })
+        })?
         .json::<TokenResponse>()
         .await
         .map_err(|_| reject::reject())?;
@@ -90,10 +140,14 @@ pub async fn handle_login(query: HashMap<String, String>) -> Result<impl Reply, 
     resp.insert("token", token.clone());
     match parse_jwt_token(&token) {
         Ok(_) => Ok(reply::json(&resp)),
-        Err(_) => return Err(reject::reject()),
+        Err(_) => Err(reject::reject()),
     }
 }
 
+/// Every request to microservices behind will be handled in this function. 
+/// The function will do authentication first to confirm the access_token is valid. 
+/// Then it will do authorization using casbin to check the request permission. 
+/// (sub, obj, act) <-> (owner/name, request path, lowercase request method)
 pub async fn handle_authenticate(
     token: String,
     method: String,
@@ -107,10 +161,20 @@ pub async fn handle_authenticate(
     let resp = CLIENT.post(format!("{}/api/login/oauth/introspect?token={}&token_type_hint=access_token&client_id={}&client_secret={}", CONFIG.endpoint, token, CONFIG.client_id, CONFIG.client_secret))
         .send()
         .await
-        .map_err(|_| reject::reject())?
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection {
+                msg: "Request for casdoor api \"/api/login/oauth/introspect\" failed".to_string()
+            })
+        })?
         .json::<ActiveResponse>()
         .await
-        .map_err(|_| reject::reject())?;
+        .map_err(|err| {
+            error!("{}", err);
+            reject::custom(CustomRejection {
+                msg: "Deserialize response from casdoor api \"/api/login/oauth/introspect\" failed".to_string()
+            })
+        })?;
 
     debug!("{:#?}", resp);
 
@@ -129,9 +193,11 @@ pub async fn handle_authenticate(
 
 /// Global exception handler function.
 /// It is always an empty response with FORBIDDEN http status.
-pub async fn err_handle(_err: Rejection) -> Result<impl Reply, Infallible> {
-    Ok(warp::reply::with_status(
-        warp::reply::reply(),
-        StatusCode::FORBIDDEN,
-    ))
+pub async fn err_handle(err: Rejection) -> Result<impl Reply, Infallible> {
+    if let Some(e) = err.find::<CustomRejection>() {
+        error!("{}", e.msg);
+        Ok(reply::with_status(reply::html(e.msg.clone()), StatusCode::BAD_GATEWAY))
+    } else {
+        Ok(reply::with_status(reply::html("".to_string()), StatusCode::FORBIDDEN))
+    }
 }
